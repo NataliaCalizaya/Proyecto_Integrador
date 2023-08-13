a58a8c544705c9758ccb0e329657f0aced930413
NataliaCalizaya <nataliacalizaya312@gmail.com>
2023-08-13 13:35:23 -0300

    six commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use (express.static (path.join(__dirname, 'public')))
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'))
 });
 app.get('/login', (req, res) => {
