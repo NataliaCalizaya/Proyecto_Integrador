a98ae2b8fee2f263ea4eba19813aebb56d72f3f3
NataliaCalizaya <nataliacalizaya312@gmail.com>
2023-08-13 08:45:38 -0300

    four commit

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const path = require('path');
 
 app.use (express.static (path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'))
 });
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
 
-app.listen ((3080), () => console.log ('servidor corriendo'))
+const port = process.env.PORT || 3080;
+app.listen (port, ()=> console.log(`Servidor corriendo en el puerto ${port}`))
 
